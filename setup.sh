b1d231bb9b330ddc35400a5c08a699ecfd130d5b
Natsuki Tanaka <n2kia4@gmail.com>
2017-03-10 21:06:09 +0900

    Update setup script to install Homebrew and Vim plugin

--- setup.sh
+++ setup.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+# Symbolic link
 for file in .??*
 do
   filepath="${PWD}/${file}"
@@ -8,5 +9,19 @@ do
   [[ "$file" == ".gitignore" ]] && continue
   [[ "$file" == ".DS_Store" ]] && continue
 
-  ln -s $filepath $HOME/$file
+  ln -sfnv $filepath $HOME/$file
 done
+echo "[OK] Symbolic link"
+
+# Install Homebrew
+which -s brew
+if [[ $? != 0 ]] ; then
+    ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+    echo "[OK] Install Homebrew"
+else
+    echo "[SKIP] Install Homebrew"
+fi
+
+# Install Vim plugin
+vim +PlugInstall +qall
+echo "[OK] Install Vim plugin"
