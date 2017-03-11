6689954d6a56b5a44301512c6c0fc97b6c4873f6
Natsuki Tanaka <n2kia4@gmail.com>
2017-03-11 20:43:20 +0900

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -14,29 +14,31 @@ done
 echo "[OK] Symbolic link"
 
 # Install Homebrew
-which -s brew
-if [[ $? != 0 ]] ; then
-    echo "Installing Homebrew..."
-    ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
-    echo "[OK] Install Homebrew"
-else
-    echo "[SKIP] Install Homebrew"
+if [ $(uname) = 'Darwin' ]; then
+    which -s brew
+    if [[ $? != 0 ]] ; then
+        echo "Installing Homebrew..."
+        ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+        echo "[OK] Install Homebrew"
+    else
+        echo "[SKIP] Install Homebrew"
+    fi
+
+    # Run brew update && brew doctor
+    echo "Run brew update..."
+    brew update
+
+    echo "Run brew doctor..."
+    brew doctor
+
+    # Install packages
+    echo "Installing packages..."
+    brew install \
+        autoconf coreutils git go openssl \
+        python python3 rbenv readline ruby-build \
+        sbt tmux vim zsh
+    echo "[OK] Install Packages"
 fi
-
-# Run brew update && brew doctor
-echo "Run brew update..."
-brew update
-
-echo "Run brew doctor..."
-brew doctor
-
-# Install packages
-echo "Installing packages..."
-brew install \
-    autoconf coreutils git go openssl \
-    python python3 rbenv readline ruby-build \
-    sbt tmux vim zsh
-echo "[OK] Install Packages"
 
 # Install Vim plugin
 vim +PlugInstall +qall
