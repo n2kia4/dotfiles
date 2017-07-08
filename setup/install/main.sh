c5e1699397815c88b8cbfbbc76051234e38bb65c
Natsuki Tanaka <n2kia4@gmail.com>
2017-07-08 18:42:07 +0900

    Add shell script to install xcode-select

--- setup/install/main.sh
+++ setup/install/main.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
+. setup/install/xcode.sh
 . setup/install/brew.sh
 . setup/install/vim.sh
---
+++ setup/install/xcode.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# Load utils
+. setup/utils.sh
+
+print_title "---Xcode---"
+
+if [ -d "$(xcode-select -p)" ]; then
+    print_warning "xcode-select already installed"
+else
+    xcode-select --install
+fi
