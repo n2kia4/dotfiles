43aabca57fa0ab55ddb6d75e4e72ecafe8f5105b
Natsuki Tanaka <n2kia4@gmail.com>
2017-12-05 00:12:01 +0900

    Add rustup installation to setup script

--- setup/install/main.sh
+++ setup/install/main.sh
@@ -2,4 +2,5 @@
 
 . setup/install/xcode.sh
 . setup/install/brew.sh
+. setup/install/rust.sh
 . setup/install/vim.sh
---
+++ setup/install/rust.sh
@@ -0,0 +1,12 @@
+#!bin/bash
+
+# Load utils
+. setup/utils.sh
+
+print_title "Rust"
+
+if type rustup > /dev/null 2>&1; then
+    print_warning "rustup: already installed"
+else
+    curl https://sh.rustup.rs -sSf | sh
+fi
