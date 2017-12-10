83ac5f421143076fc4fac3444f77f77f91af9148
Natsuki Tanaka <n2kia4@gmail.com>
2017-12-11 01:35:10 +0900

    Add racer and rustfmt installation to setup script
    This is for rust.vim and vim-racer

--- setup/install/rust.sh
+++ setup/install/rust.sh
@@ -9,4 +9,6 @@ if type rustup > /dev/null 2>&1; then
     print_warning "rustup: already installed"
 else
     curl https://sh.rustup.rs -sSf | sh
+    cargo install racer rustfmt
+    rustup component add rust-src
 fi
